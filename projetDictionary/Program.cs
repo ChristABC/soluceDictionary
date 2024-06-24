8b314f5690d23cf81ce08306e2b407d4b4a8ee5c
Christ ASSANA <christassana@gmail.com>
2024-06-24 10:45:48 +0200

    Ajoutez des fichiers projet.

---
+++ projetDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class MainProgram
+{
+    public static void Main(string[] args)
+    {
+        Dictionary<char,string> dic = new Dictionary<char,string>();
+        dic['a'] = "alphabet";
+        dic['b'] = "balade";
+        dic['c'] = "croisade";
+
+        dic.Remove('c');
+
+        foreach (char c in dic.Keys)
+        {
+            Console.WriteLine(c);
+        }
+
+    }
+}
+
